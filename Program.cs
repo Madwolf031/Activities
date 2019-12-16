a176e0d47e4d19e82481106ebfe10ebdf3d5e588
Madwolf031 <57928242+Madwolf031@users.noreply.github.com>
2019-12-17 06:25:59 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Simple_Calculator__Lamparas_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer;
+            string useoper;
+
+
+            Console.WriteLine("Console Simple Calculator\n\n\n");
+
+            Console.WriteLine("Enter the first number");
+            firstnum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            secondnum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator: + - x /");
+            useoper = Console.ReadLine();
+
+
+            if (useoper == "+")
+			{
+                answer = firstnum + secondnum;
+                Console.WriteLine("\nAnswer:" + answer);
+            }
+
+            else if (useoper == "-")
+			{
+                answer = firstnum - secondnum;
+                Console.WriteLine("\nAnswer:" + answer);
+            }
+
+
+            else if (useoper == "*")
+			{
+                answer = firstnum + secondnum;
+                Console.WriteLine("\nAnswer:" + answer);
+            }
+
+            else if (useoper == "/")
+			{
+                answer = firstnum + secondnum;
+                Console.WriteLine("\nAnswer:" + answer);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input of operator");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+        }
+    }
+}
